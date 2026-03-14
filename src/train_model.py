916426e76c9f5975baabdf2cf9bd502535615f35
laouineyoussef <laouineyoussef1@gmail.com>
2026-03-14 22:08:34 +0000

    l

--- src/train_model.py
+++ src/train_model.py
@@ -9,7 +9,7 @@ from sklearn.ensemble import RandomForestClassifier
 from xgboost import XGBClassifier
 from lightgbm import LGBMClassifier
 from sklearn.metrics import roc_auc_score, f1_score
-from src.data_processing import load_data, handle_outliers, optimize_memory
+from src.data_processing import load_data, handle_outliers, optimize_memory,prepare_data
 
 
 MODELS = {
